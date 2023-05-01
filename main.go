8d0e43577ac305143fcdf67d75d5a710dab618f7
haim-kermany <haim.kermany@gmail.com>
2023-05-01 14:34:36 +0300

    adding NI

--- main.go
+++ main.go
@@ -77,12 +77,30 @@ func NewDrawioIconElement(etype string, x uint, y uint, w uint, h uint, id uint,
 	return &DrawioIconElement{DrawioLocatedElement{DrawioElement{etype, id}, pid, x, y, name}}
 }
 
+// ///////////////////////////////////////////////////////////////////////////////////////////////
+type DrawioNetworkInterfaceElement struct {
+	DrawioIconElement
+	floating_ip string
+	svi         string
+}
+
+func (di DrawioNetworkInterfaceElement) GetFip() string {
+	return di.floating_ip
+}
+
+func (di DrawioNetworkInterfaceElement) GetSvi() string {
+	return di.svi
+}
+
+func NewDrawioNetworkInterfaceElement(etype string, x uint, y uint, w uint, h uint, id uint, pid uint, name string, fip string, svi string) *DrawioNetworkInterfaceElement {
+	return &DrawioNetworkInterfaceElement{DrawioIconElement{DrawioLocatedElement{DrawioElement{etype, id}, pid, x, y, name}}, fip, svi}
+}
+
 // //////////////////////////////////////////////////////////////////////////////
 type DrawioConnectElement struct {
 	DrawioElement
 	srcId uint
 	dstId uint
-	name  string
 }
 
 func (di DrawioElement) GetIP() string {
@@ -104,27 +122,27 @@ func main() {
 		NewDrawioSquereElement("vpc", 160, 40, 1720, 960, 1230, 895, "noname"),
 		NewDrawioSquereElement("zone", 40, 40, 520, 880, 1235, 1230, "us-south-3"),
 		NewDrawioSquereElement("subnet", 160, 600, 320, 240, 1240, 1235, "ky-testenv-transit-subnet-3"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1245, 1240, "transit-2-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1245, 1240, "transit-2-instance-ky", "", "vsi_name"),
 		NewDrawioSquereElement("subnet", 160, 40, 320, 240, 1250, 1235, "ky-testenv-edge-subnet-3"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1255, 1250, "edge-2-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1255, 1250, "edge-2-instance-ky", "", "vsi_name"),
 		NewDrawioSquereElement("subnet", 160, 320, 320, 240, 1260, 1235, "ky-testenv-private-subnet-3"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1265, 1260, "private-2-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1265, 1260, "private-2-instance-ky", "", "vsi_name"),
 		NewDrawioIconElement("gateway", 50.0, 130.0, 60, 60, 1270, 1235, "ky-testenv-gateway-3"),
 		NewDrawioSquereElement("zone", 600, 40, 520, 880, 1275, 1230, "us-south-2"),
 		NewDrawioSquereElement("subnet", 160, 600, 320, 240, 1280, 1275, "ky-testenv-transit-subnet-2"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1285, 1280, "transit-1-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1285, 1280, "transit-1-instance-ky", "fip_name", "vsi_name"),
 		NewDrawioSquereElement("subnet", 160, 40, 320, 240, 1290, 1275, "ky-testenv-edge-subnet-2"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1295, 1290, "edge-1-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1295, 1290, "edge-1-instance-ky", "", "vsi_name"),
 		NewDrawioSquereElement("subnet", 160, 320, 320, 240, 1300, 1275, "ky-testenv-private-subnet-2"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1305, 1300, "private-1-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1305, 1300, "private-1-instance-ky", "", "vsi_name"),
 		NewDrawioIconElement("gateway", 50.0, 410.0, 60, 60, 1310, 1275, "ky-testenv-gateway-2"),
 		NewDrawioSquereElement("zone", 1160, 40, 520, 880, 1315, 1230, "us-south-1"),
 		NewDrawioSquereElement("subnet", 160, 320, 320, 240, 1320, 1315, "ky-testenv-private-subnet-1"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1325, 1320, "private-0-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1325, 1320, "private-0-instance-ky", "fip_name", "vsi_name"),
 		NewDrawioSquereElement("subnet", 160, 40, 320, 240, 1330, 1315, "ky-testenv-edge-subnet-1"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1335, 1330, "edge-0-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1335, 1330, "edge-0-instance-ky", "", "vsi_name"),
 		NewDrawioSquereElement("subnet", 160, 600, 320, 240, 1340, 1315, "ky-testenv-transit-subnet-1"),
-		NewDrawioIconElement("ni_vsi", 130.0, 90.0, 60, 60, 1345, 1340, "transit-0-instance-ky"),
+		NewDrawioNetworkInterfaceElement("ni", 130.0, 90.0, 60, 60, 1345, 1340, "transit-0-instance-ky", "", "vsi_name"),
 		NewDrawioIconElement("gateway", 50.0, 690.0, 60, 60, 1350, 1315, "ky-testenv-gateway-1"),
 		NewDrawioSquereElement("sg", 290.0, 120, 1260.0, 160, 1355, 1230, "ky-testenv-default-sg"),
 		NewDrawioSquereElement("sg", 290.0, 400, 1260.0, 160, 1360, 1230, "ky-testenv-default-sg"),
